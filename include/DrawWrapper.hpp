a9651a8134e24bca82e3eb2437f894048d41a8ff
syoch <hacksyoch@gmail.com>
2021-02-24 23:44:41 +0900

    fix: change arguments of gui::draw_translate

--- include/DrawWrapper.hpp
+++ include/DrawWrapper.hpp
@@ -11,7 +11,7 @@ namespace gui
     void draw_rect(Point start, Point end);
     void draw_triangle(Point A, Point B, Point C);
     void draw_text(int row, int column, char *);
-    void draw_translate(int row, int column, char *);
+    void draw_translate(int x, int y);
 }
 
 #endif
