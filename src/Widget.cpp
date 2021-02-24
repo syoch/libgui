49f78d2b0a8bc2d4ab75ee1bd98a6aa754473e9c
syoch <hacksyoch@gmail.com>
2021-02-24 18:32:30 +0900

    update: add prototype declare of Widget::draw*

---
+++ src/Widget.cpp
@@ -0,0 +1,14 @@
+#include <Widget.hpp>
+
+void gui::Widget::draw_line(Point start, Point end)
+{
+}
+void gui::Widget::draw_rect(Point start, Point end)
+{
+}
+void gui::Widget::draw_triangle(Point A, Point B, Point C)
+{
+}
+void gui::Widget::draw_text(int row, int column, char *)
+{
+}
