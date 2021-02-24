169d40c46b7c4560a36f138f75e296187842aafd
syoch <hacksyoch@gmail.com>
2021-02-25 00:50:27 +0900

    update: add some prototype declare of widget

--- src/Widget.cpp
+++ src/Widget.cpp
@@ -21,3 +21,12 @@ void gui::Widget::draw_triangle(Point A, Point B, Point C)
 void gui::Widget::draw_text(int row, int column, char *)
 {
 }
+void gui::Widget::onTouch(int x, int y)
+{
+}
+void gui::Widget::onButton(uint32_t key)
+{
+}
+void gui::Widget::draw()
+{
+}
