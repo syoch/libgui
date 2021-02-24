44c831b5e0340f6f6ce23f26ca6bc21cde4d38f5
syoch <hacksyoch@gmail.com>
2021-02-24 17:30:15 +0900

    update: add widget

---
+++ include/Widget.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef LIBGUI_WIDGET
+#define LIBGUI_WIDGET
+#include "Point.hpp"
+#include "Size.hpp"
+
+namespace gui
+{
+    class Widget
+    {
+    private:
+        Point pos;
+        Size size;
+
+    public:
+        Widget(int x, int y, int w, int h);
+
+        void onTouch(int x, int y);
+        void onButton(int x, int y);
+
+        void draw_line(Point start, Point end);
+        void draw_rect(Point start, Point end);
+        void draw_triangle(Point A, Point B, Point C);
+        void draw_text(int row, int column, char *);
+    };
+}
+
+#endif
