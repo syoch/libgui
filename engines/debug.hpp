36e89cd293ca947db1cf9991a63848b37dc98d06
syoch <hacksyoch@gmail.com>
2021-02-25 19:03:49 +0900

    refactor: update gui

--- engines/debug.hpp
+++ engines/debug.hpp
@@ -2,45 +2,48 @@
 #include <DrawWrapper.hpp>
 #include <iostream>
 
-void gui::draw_line(gui::Point start, gui::Point end)
+namespace gui
 {
-    std::wcout << "Draw line:"
-               << "(" << start.x << ", " << start.y << ")"
-               << "to"
-               << "(" << end.x << ", " << end.y << ")"
-               << std::endl;
-}
+    void draw_line(gui::Point start, gui::Point end)
+    {
+        std::wcout << "Draw line:"
+                   << "(" << start.x << ", " << start.y << ")"
+                   << "to"
+                   << "(" << end.x << ", " << end.y << ")"
+                   << std::endl;
+    }
 
-void gui::draw_rect(gui::Point start, gui::Point end)
-{
-    std::wcout << "Draw rect:"
-               << "(" << start.x << ", " << start.y << ")"
-               << "to"
-               << "(" << end.x << ", " << end.y << ")"
-               << std::endl;
-}
+    void draw_rect(gui::Point start, gui::Point end)
+    {
+        std::wcout << "Draw rect:"
+                   << "(" << start.x << ", " << start.y << ")"
+                   << "to"
+                   << "(" << end.x << ", " << end.y << ")"
+                   << std::endl;
+    }
 
-void gui::draw_text(int row, int column, char *text)
-{
-    std::wcout << "Draw text:"
-               << row << "," << column
-               << " [" << text << "]"
-               << std::endl;
-}
-void gui::draw_translate(int x, int y)
-{
-    std::wcout << "Draw tran:"
-               << x << ", " << y
-               << std::endl;
-}
-void gui::draw_triangle(gui::Point A, gui::Point B, gui::Point C)
-{
-    std::wcout << "Draw tria:"
-               << "(" << A.x << ", " << A.y << ")"
-               << ", "
-               << "(" << B.x << ", " << B.y << ")"
-               << ", "
-               << "(" << C.x << ", " << C.y << ")"
-               << ", "
-               << std::endl;
+    void draw_text(int row, int column, char *text)
+    {
+        std::wcout << "Draw text:"
+                   << row << "," << column
+                   << " [" << text << "]"
+                   << std::endl;
+    }
+    void draw_translate(int x, int y)
+    {
+        std::wcout << "Draw tran:"
+                   << x << ", " << y
+                   << std::endl;
+    }
+    void draw_triangle(gui::Point A, gui::Point B, gui::Point C)
+    {
+        std::wcout << "Draw tria:"
+                   << "(" << A.x << ", " << A.y << ")"
+                   << ", "
+                   << "(" << B.x << ", " << B.y << ")"
+                   << ", "
+                   << "(" << C.x << ", " << C.y << ")"
+                   << ", "
+                   << std::endl;
+    }
 }
