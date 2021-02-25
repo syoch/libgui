4af263d5d3143da3a50ec310f44a359075fa4e0e
syoch <hacksyoch@gmail.com>
2021-02-25 23:37:46 +0900

    update: add ctor of Widget

--- include/Widget.hpp
+++ include/Widget.hpp
@@ -3,6 +3,7 @@
 #define LIBGUI_WIDGET
 #include <WidgetBase.hpp>
 #include <Container.hpp>
+#include <Root.hpp>
 namespace gui
 {
     class Widget : public WidgetBase
@@ -12,6 +13,7 @@ namespace gui
 
     public:
         Widget(gui::Container _super, int x, int y, int w, int h);
+        Widget(gui::Root _super, int x, int y, int w, int h);
     };
 }
 
--- src/Widget.cpp
+++ src/Widget.cpp
@@ -6,3 +6,9 @@ gui::Widget::Widget(gui::Container _super, int x, int y, int w, int h)
     pos = ((Point){x, y});
     size = ((Size){w, h});
 }
+gui::Widget::Widget(gui::Root _super, int x, int y, int w, int h)
+{
+    super = _super;
+    pos = ((Point){x, y});
+    size = ((Size){w, h});
+}
