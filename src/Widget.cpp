fa9770dcdf7469e6004027f2fee2fa4a9f5db49a
syoch <hacksyoch@gmail.com>
2021-02-26 00:07:08 +0900

    fix: Widget ctor compile

--- src/Widget.cpp
+++ src/Widget.cpp
@@ -5,5 +5,5 @@ gui::Widget::Widget(gui::Container &_super, int x, int y, int w, int h)
     super = _super;
     pos = ((Point){x, y});
     size = ((Size){w, h});
-    _super.pushWidget(*this);
+    _super.childrens.emplace_back(*this);
 }
