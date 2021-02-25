091404368b8c44cae4383e641fa5734d2e2134eb
syoch <hacksyoch@gmail.com>
2021-02-26 00:03:34 +0900

    update: remove pushWidget

--- include/Container.hpp
+++ include/Container.hpp
@@ -9,12 +9,10 @@ namespace gui
 {
     class Container : public WidgetBase
     {
-    private:
-        std::vector<WidgetBase> childrens;
-
     public:
         void _draw();
-        void pushWidget(WidgetBase widget);
+
+        std::vector<WidgetBase> childrens;
     };
 }
 
--- src/Container.cpp
+++ src/Container.cpp
@@ -1,9 +1,5 @@
 #include <Container.hpp>
 
-void gui::Container::pushWidget(gui::WidgetBase widget)
-{
-    childrens.push_back(widget);
-}
 void gui::Container::_draw()
 {
     for (auto elem : childrens)
