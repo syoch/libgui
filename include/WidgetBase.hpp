8d111157da4f7e391e7117b44f68361b98790871
syoch <hacksyoch@gmail.com>
2021-02-25 20:19:40 +0900

    update: add WidgetBase

---
+++ include/WidgetBase.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef LIBGUI_WIDGET_BASE
+#define LIBGUI_WIDGET_BASE
+
+#include <inttypes.h>
+
+#include "Point.hpp"
+#include "Size.hpp"
+
+namespace gui
+{
+    class WidgetBase
+    {
+    private:
+        void draw_line(Point start, Point end);
+        void draw_rect(Point start, Point end);
+        void draw_triangle(Point A, Point B, Point C);
+        void draw_text(int row, int column, char *);
+        void draw();
+
+    public:
+        void _draw();
+        virtual void onTouch(int x, int y);
+        virtual void onButton(uint32_t key);
+
+        Point pos;
+        Size size;
+    };
+}
+
+#endif
