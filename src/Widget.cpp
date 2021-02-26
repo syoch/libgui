b3176f126c14e91c219e706ba429b0847fc431ee
syoch <hacksyoch@gmail.com>
2021-02-26 17:34:32 +0900

    fix: failed to compile Widget::Widget

--- src/Widget.cpp
+++ src/Widget.cpp
@@ -1,8 +1,8 @@
 #include <Widget.hpp>
 
 gui::Widget::Widget(gui::Container &_super, int x, int y, int w, int h)
+    : super(_super)
 {
-    super = _super;
     pos = ((Point){x, y});
     size = ((Size){w, h});
     _super.childrens.emplace_back(*this);
