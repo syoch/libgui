d849003007f2f5cebc04e1dc5bc200e0a0bbd31f
syoch <hacksyoch@gmail.com>
2021-02-26 17:19:56 +0900

    update: change  Widget Container declare type
    include to foward declare

--- include/Widget.hpp
+++ include/Widget.hpp
@@ -2,14 +2,15 @@
 #ifndef LIBGUI_WIDGET
 #define LIBGUI_WIDGET
 #include <WidgetBase.hpp>
-#include <Container.hpp>
 #include <Root.hpp>
+
 namespace gui
 {
+    class Container;
     class Widget : public WidgetBase
     {
     private:
-        Container super;
+        Container &super;
 
     public:
         Widget(gui::Container &_super, int x, int y, int w, int h);
