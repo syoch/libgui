b8dbb0c89c8647ba490d3338b22ab26184ea06f0
syoch <hacksyoch@gmail.com>
2021-02-26 23:54:08 +0900

    update: implement some draw function

--- src/Root.cpp
+++ src/Root.cpp
@@ -1,19 +1,25 @@
 #include <Root.hpp>
+#include <DrawWrapper.hpp>
 
 void GUI::Root::draw_line(GUI::Point start, GUI::Point end)
 {
+    GUI::draw_line(start, end);
 }
 void GUI::Root::draw_rect(GUI::Point start, GUI::Point end)
 {
+    GUI::draw_rect(start, end);
 }
 void GUI::Root::draw_triangle(GUI::Point A, GUI::Point B, GUI::Point C)
 {
+    GUI::draw_triangle(A, B, C);
 }
 void GUI::Root::draw_text(int row, int column, char *text)
 {
+    GUI::draw_text(row, column, text);
 }
 void GUI::Root::draw_translate(int x, int y)
 {
+    GUI::draw_translate(x, y);
 }
 void GUI::Root::onTouch(int x, int y)
 {
