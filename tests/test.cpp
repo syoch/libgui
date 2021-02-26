120cdd958c0fd7f87abe56eaee37880ae9d13786
syoch <hacksyoch@gmail.com>
2021-02-27 00:45:32 +0900

    update: add custom widget test

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,11 +1,26 @@
 #include <gui.hpp>
 #include <engines/debug.hpp>
 using namespace GUI;
+class Btn : public Widget
+{
+private:
+    void draw()
+    {
+        draw_rect((Point){0, 0}, (Point){size.width / 2, size.height / 2});
+    }
+
+public:
+    using Widget::Widget;
+    void onTouch(int x, int y)
+    {
+        std::wcout << "touched btn" << std::endl;
+    }
+};
 int main(int argc, char const *argv[])
 {
     Root root;
 
-    Widget(root, 0, 0, 100, 100);
+    Btn(root, 0, 0, 100, 100);
 
     root._draw();
     return 0;
