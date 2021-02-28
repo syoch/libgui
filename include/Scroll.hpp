34bda9957f61c56c7d0c4dea187465b4ea8c797e
syoch <hacksyoch@gmail.com>
2021-02-28 16:50:05 +0900

    update: define scroll

---
+++ include/Scroll.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef LIBGUI_SCROLL
+#define LIBGUI_SCROLL
+
+#include <vector>
+#include "ContainerBase.hpp"
+
+namespace GUI
+{
+    class Scroll : public ContainerBase
+    {
+        void draw_line(GUI::Point start, GUI::Point end);
+        void draw_rect(GUI::Point start, GUI::Point end);
+        void draw_triangle(GUI::Point A, GUI::Point B, GUI::Point C);
+        void draw_text(int row, int column, char *);
+        void draw_translate(int x, int y);
+
+    public:
+        Scroll();
+        void down(int dy);
+        void up(int dy);
+
+    private:
+        int yOffset;
+    };
+}
+
+#endif
