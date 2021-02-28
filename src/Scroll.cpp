8739e5847a7df7700a62990bd916e353f81bae21
syoch <hacksyoch@gmail.com>
2021-02-28 21:18:42 +0900

    update: add WidgetBase and ContainerBase ctor

--- src/Scroll.cpp
+++ src/Scroll.cpp
@@ -1,7 +1,7 @@
 #include <Scroll.hpp>
 
 GUI::Scroll::Scroll(GUI::ContainerBase &super, int x, int y, int w, int h)
-    : GUI::Container(super, x, y, w, h), yOffset(0)
+    : GUI::Container(super, x, y, w, h), yOffset(0), WidgetBase(super, x, y, w, h), ContainerBase(x, y, w, h)
 {
 }
 void GUI::Scroll::up(int dy)
