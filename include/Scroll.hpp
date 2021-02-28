1013b592799f1d1fbf68ef4b675b2a860875547d
syoch <hacksyoch@gmail.com>
2021-02-28 22:01:42 +0900

    update: implement(?) Scroll::draw_*

--- include/Scroll.hpp
+++ include/Scroll.hpp
@@ -9,9 +9,9 @@ namespace GUI
 {
     class Scroll : public Container
     {
-        void draw_line(GUI::Point start, GUI::Point end);
-        void draw_rect(GUI::Point start, GUI::Point end);
-        void draw_triangle(GUI::Point A, GUI::Point B, GUI::Point C);
+        void draw_line(Point start, Point end);
+        void draw_rect(Point start, Point end);
+        void draw_triangle(Point A, Point B, Point C);
         void draw_text(int row, int column, char *);
         void draw_translate(int x, int y);
 
--- src/Scroll.cpp
+++ src/Scroll.cpp
@@ -1,4 +1,5 @@
 #include <Scroll.hpp>
+#include <WidgetBase.hpp>
 
 GUI::Scroll::Scroll(GUI::ContainerBase &super, int x, int y, int w, int h)
     : GUI::Container(super, x, y, w, h), yOffset(0), WidgetBase(super, x, y, w, h), ContainerBase(x, y, w, h)
@@ -11,4 +12,24 @@ void GUI::Scroll::up(int dy)
 void GUI::Scroll::down(int dy)
 {
     yOffset += dy;
+}
+void GUI::Scroll::draw_line(Point start, Point end)
+{
+    super.draw_line(start, end);
+}
+void GUI::Scroll::draw_rect(Point start, Point end)
+{
+    super.draw_rect(start, end);
+}
+void GUI::Scroll::draw_triangle(Point A, Point B, Point C)
+{
+    super.draw_triangle(A, B, C);
+}
+void GUI::Scroll::draw_text(int row, int column, char *text)
+{
+    super.draw_text(row, column, text);
+}
+void GUI::Scroll::draw_translate(int x, int y)
+{
+    super.draw_translate(x, y);
 }
