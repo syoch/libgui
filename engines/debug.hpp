97e50722d34d9286678be9f3c252a5df83bdab1c
syoch <hacksyoch@gmail.com>
2021-03-18 07:30:25 +0900

    fix: render declare

--- engines/debug.hpp
+++ engines/debug.hpp
@@ -4,77 +4,90 @@
 
 namespace GUI
 {
-    const bool isRelative = true;
-    void draw_begin()
+    namespace render
     {
-        std::wcout << "draw begin" << std::endl;
-    }
-    void draw_end()
-    {
-        std::wcout << "draw end" << std::endl;
-    }
-    void draw_line(GUI::DrawPoint start, GUI::DrawPoint end)
-    {
-        std::wcout << "Draw line: "
-                   << "(" << start.point.x << ", " << start.point.y << ")"
-                   << "[" << start.color.r << ", " << start.color.b << ", " << start.color.g << ", " << start.color.a << "] "
-                   << "to"
-                   << "(" << end.point.x << ", " << end.point.y << ")"
-                   << "[" << end.color.r << ", " << end.color.b << ", " << end.color.g << ", " << end.color.a << "] "
-                   << std::endl;
-    }
+        const bool isRelative = true;
+        void begin()
+        {
+            std::wcout << "draw begin" << std::endl;
+        }
+        void end()
+        {
+            std::wcout << "draw end" << std::endl;
+        }
+        void line(DrawInfo start, DrawInfo end)
+        {
 
-    void draw_rect(GUI::DrawPoint A, GUI::DrawPoint B, GUI::DrawPoint C, GUI::DrawPoint D)
-    {
-        std::wcout << "Draw rect: "
-                   << "(" << A.point.x << ", " << A.point.y << ")"
-                   << "[" << A.color.r << ", " << A.color.b << ", " << A.color.g << ", " << A.color.a << "] "
-                   << ", "
-                   << "(" << B.point.x << ", " << B.point.y << ")"
-                   << "[" << B.color.r << ", " << B.color.b << ", " << B.color.g << ", " << B.color.a << "] "
-                   << ", "
-                   << "(" << C.point.x << ", " << C.point.y << ")"
-                   << "[" << C.color.r << ", " << C.color.b << ", " << C.color.g << ", " << C.color.a << "] "
-                   << ", "
-                   << "(" << D.point.x << ", " << D.point.y << ")"
-                   << "[" << D.color.r << ", " << D.color.b << ", " << D.color.g << ", " << D.color.a << "] "
-                   << std::endl;
-    }
+            std::wcout << "Draw line: "
+                       << "(" << start.pos.x << ", " << start.pos.y << ")"
+                       << "[" << start.color << ", "
+                       << "] "
+                       << "to"
+                       << "(" << end.pos.x << ", " << end.pos.y << ")"
+                       << "[" << end.color << ", "
+                       << "] "
+                       << std::endl;
+        }
 
-    void draw_text(int row, int column, char *text, Color color)
-    {
-        std::wcout << "Draw text: "
-                   << row << "," << column << " "
-                   << "[" << text << "] "
-                   << "[" << color.r << ", " << color.b << ", " << color.g << ", " << color.a << "] "
-                   << std::endl;
-    }
-    void draw_textShadow(int row, int column, char *text, Color color)
-    {
-        std::wcout << "Draw Stex: "
-                   << row << "," << column << " "
-                   << "[" << text << "] "
-                   << "[" << color.r << ", " << color.b << ", " << color.g << ", " << color.a << "] "
-                   << std::endl;
-    }
-    void draw_translate(float x, float y)
-    {
-        std::wcout << "Draw tran: "
-                   << x << ", " << y
-                   << std::endl;
-    }
-    void draw_triangle(GUI::DrawPoint A, GUI::DrawPoint B, GUI::DrawPoint C)
-    {
-        std::wcout << "Draw tria: "
-                   << "(" << A.point.x << ", " << A.point.y << ")"
-                   << "[" << A.color.r << ", " << A.color.b << ", " << A.color.g << ", " << A.color.a << "] "
-                   << ", "
-                   << "(" << B.point.x << ", " << B.point.y << ")"
-                   << "[" << B.color.r << ", " << B.color.b << ", " << B.color.g << ", " << B.color.a << "] "
-                   << ", "
-                   << "(" << C.point.x << ", " << C.point.y << ")"
-                   << "[" << C.color.r << ", " << C.color.b << ", " << C.color.g << ", " << C.color.a << "] "
-                   << ", "
-                   << std::endl;
-    }
+        void rect(DrawInfo A, DrawInfo B, DrawInfo C, DrawInfo D)
+        {
+            std::wcout << "Draw rect: "
+                       << "(" << A.pos.x << ", " << A.pos.y << ")"
+                       << "[" << A.color << ", "
+                       << "] "
+                       << ", "
+                       << "(" << B.pos.x << ", " << B.pos.y << ")"
+                       << "[" << B.color << ", "
+                       << "] "
+                       << ", "
+                       << "(" << C.pos.x << ", " << C.pos.y << ")"
+                       << "[" << C.color << ", "
+                       << "] "
+                       << ", "
+                       << "(" << D.pos.x << ", " << D.pos.y << ")"
+                       << "[" << D.color << ", "
+                       << "] "
+                       << std::endl;
+        }
+
+        void text(int row, int column, char *text, Color color)
+        {
+            std::wcout << "Draw text: "
+                       << row << "," << column << " "
+                       << "[" << text << "] "
+                       << "[" << color << "]" << std::endl;
+            ;
+        }
+        void textShadow(int row, int column, char *text, Color color)
+        {
+            std::wcout << "Draw Stex: "
+                       << row << "," << column << " "
+                       << "[" << text << "] "
+                       << "[" << color << "]" << std::endl;
+        }
+        void translate(float x, float y)
+        {
+            std::wcout << "Draw tran: "
+                       << x << ", " << y
+                       << std::endl;
+        }
+        void triangle(DrawInfo A, DrawInfo B, DrawInfo C)
+        {
+            std::wcout << "Draw tria: "
+                       << "(" << A.pos.x << ", " << A.pos.y << ")"
+                       << "[" << A.color << ", "
+                       << "] "
+                       << ", "
+                       << "(" << B.pos.x << ", " << B.pos.y << ")"
+                       << "[" << B.color << ", "
+                       << "] "
+                       << ", "
+                       << "(" << C.pos.x << ", " << C.pos.y << ")"
+                       << "[" << C.color << ", "
+                       << "] "
+                       << ", "
+                       << std::endl;
+        }
+    } // namespace render
+
 }
