23e352be4e738ae4c94ee90d2e5fdd4aa163defe
syoch <hacksyoch@gmail.com>
2021-03-18 00:00:01 +0900

    update: define render

--- include/DrawWrapper.hpp
+++ include/DrawWrapper.hpp
@@ -2,22 +2,23 @@
 #ifndef LIBGUI_DRAW_WRAPPER
 #define LIBGUI_DRAW_WRAPPER
 
-#include <vector>
-#include "Widget.hpp"
+#include <Types.hpp>
 
 namespace GUI
 {
-    extern const bool isRelative;
-    void draw_begin();
-    void draw_end();
-    void draw_line(DrawPoint start, DrawPoint end);
-    void draw_rect(DrawPoint start, DrawPoint end);
-    void draw_rect(DrawPoint A, DrawPoint B, DrawPoint C, DrawPoint D);
-    void draw_triangle(DrawPoint A, DrawPoint B, DrawPoint C);
-    void draw_text(int row, int column, char *, Color color);
-    void draw_textShadow(int row, int column, char *, Color color);
-    void draw_translate(float x, float y);
-    void _draw_translate(float x, float y);
+    namespace render
+    {
+        extern const bool isRelative;
+
+        bool init();
+        bool exit();
+        bool text(DrawInfo, Str);
+        bool rect2(DrawInfo, DrawInfo);
+        bool rect4(DrawInfo, DrawInfo, DrawInfo, DrawInfo);
+        bool triangle(DrawInfo, DrawInfo, DrawInfo);
+        bool line(DrawInfo, DrawInfo);
+        bool circle(DrawInfo, DrawInfo, int);
+    } // namespace render
 }
 
 #endif
