56573c2284b613e54f7881560fe354a5e8fb2e4c
syoch <hacksyoch@gmail.com>
2021-03-18 06:56:28 +0900

    update: add configure script (python)

---
+++ config.py
@@ -0,0 +1,30 @@
+# Type Config
+str_t = "std::wstring"
+color_t = "uint32_t"
+input_t = "uint32_t"
+vec_t = "std::vector"
+# includes
+requiredHeaders = [
+    "<vector>"
+]
+
+
+def writeConfig():
+    fp = open("include/config.h", "w")
+    fp.writelines([
+        f"#pragma once",
+        f"#ifndef LIBGUI_CONFIG",
+        f"#define LIBGUI_CONFIG",
+        f"",
+        *[f"#include {a}" for a in requiredHeaders],
+        f"",
+        f"namespace GUI",
+        f"{{",
+        f"    using Str = {str_t};",
+        f"    using Color = {color_t};",
+        f"    using InputData = {input_t};",
+        f"    using Vector = {vec_t};",
+        f"}} // namespace GUI",
+        f"",
+        f"#endif"
+    ])
