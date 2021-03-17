8f0f0ff1fe1ca4da6a2f4aeb7dca8dd5a6e25310
syoch <hacksyoch@gmail.com>
2021-03-18 06:43:53 +0900

    update: declare Container

---
+++ include/Container.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef LIBGUI_CONTAINER
+#define LIBGUI_CONTAINER
+
+#include <functional>
+
+#include <Widget.hpp>
+
+namespace GUI
+{
+    class Container : public Widget
+    {
+    public:
+        using Widget::Widget;
+        void draw() override;
+        void ontouch(Pos &pos) override;
+        void oninput(InputData &input) override;
+    };
+} // namespace GUI
+
+#endif
