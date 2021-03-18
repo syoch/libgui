36315d0cd36b18807a7bc0e6893b97ed0d927f77
syoch <hacksyoch@gmail.com>
2021-03-18 14:29:52 +0900

    update: implement Container::ontouch

--- src/Container.cpp
+++ src/Container.cpp
@@ -14,4 +14,17 @@ namespace GUI
             widget.get().draw();
         }
     }
+    void Container::ontouch(Pos &data)
+    {
+        for (auto &&widget : widgets)
+        {
+            if ((widget.get().pos.x <= data.x && data.x <= widget.get().pos.x + widget.get().size.w) ||
+                (widget.get().pos.y <= data.y && data.y <= widget.get().pos.y + widget.get().size.h))
+            {
+                data.x -= widget.get().pos.x;
+                data.y -= widget.get().pos.y;
+                widget.get().ontouch(data);
+            }
+        }
+    }
 } // namespace GUI
