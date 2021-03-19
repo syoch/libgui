bf2a44d26f9caed62c356727d5db81461a5dc963
syoch <hacksyoch@gmail.com>
2021-03-19 16:07:14 +0900

    update: add defaultConverter (in utils)

--- include/utils.hpp
+++ include/utils.hpp
@@ -2,11 +2,13 @@
 #ifndef LIBGUI_UTILS_HPP
 #define LIBGUI_UTILS_HPP
 
+#include <Types.hpp>
+
 namespace GUI
 {
     namespace utils
     {
-
+        Pos &defaultConverter(Pos &);
     } // namespace utils
 
 } // namespace GUI
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,1 +1,8 @@
 #include <utils.hpp>
+
+using namespace GUI;
+
+Pos &GUI::utils::defaultConverter(Pos &pos)
+{
+    return pos;
+}
