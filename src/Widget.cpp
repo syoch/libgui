fb40196784a2c5d2205c3ad5455b0865727dfa0c
syoch <hacksyoch@gmail.com>
2021-03-21 11:39:02 +0900

    fix: add self pos in wrapper of widget

--- src/Widget.cpp
+++ src/Widget.cpp
@@ -22,39 +22,67 @@ namespace GUI
     bool Widget::text(DrawInfo a, Str str)
     {
         converter(a.pos);
+        a.pos.x += pos.x;
+        a.pos.y += pos.y;
         return render::text(a, str);
     }
     bool Widget::rect2(DrawInfo a, DrawInfo b)
     {
         converter(a.pos);
+        a.pos.x += pos.x;
+        a.pos.y += pos.y;
         converter(b.pos);
+        b.pos.x += pos.x;
+        a.pos.y += pos.y;
         return render::rect2(a, b);
     }
     bool Widget::rect4(DrawInfo a, DrawInfo b, DrawInfo c, DrawInfo d)
     {
         converter(a.pos);
+        a.pos.x += pos.x;
+        a.pos.y += pos.y;
         converter(b.pos);
+        b.pos.x += pos.x;
+        a.pos.y += pos.y;
         converter(c.pos);
+        c.pos.x += pos.x;
+        a.pos.y += pos.y;
         converter(d.pos);
+        d.pos.x += pos.x;
+        a.pos.y += pos.y;
         return render::rect4(a, b, c, d);
     }
     bool Widget::triangle(DrawInfo a, DrawInfo b, DrawInfo c)
     {
         converter(a.pos);
+        a.pos.x += pos.x;
+        a.pos.y += pos.y;
         converter(b.pos);
+        b.pos.x += pos.x;
+        a.pos.y += pos.y;
         converter(c.pos);
+        c.pos.x += pos.x;
+        a.pos.y += pos.y;
         return render::triangle(a, b, c);
     }
     bool Widget::line(DrawInfo a, DrawInfo b)
     {
         converter(a.pos);
+        a.pos.x += pos.x;
+        a.pos.y += pos.y;
         converter(b.pos);
+        b.pos.x += pos.x;
+        a.pos.y += pos.y;
         return render::line(a, b);
     }
     bool Widget::circle(DrawInfo a, DrawInfo b)
     {
         converter(a.pos);
+        a.pos.x += pos.x;
+        a.pos.y += pos.y;
         converter(b.pos);
+        b.pos.x += pos.x;
+        a.pos.y += pos.y;
         return render::circle(a, b);
     }
 } // namespace GUI
