469d99ff26de2f7cc4bec7587e10200e3c6c130f
syoch <hacksyoch@gmail.com>
2021-04-01 19:46:31 +0900

    update: baseTypes.hpp

---
+++ include/GUI/baseTypes.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef LIBGUI_BASETYPES
+
+namespace GUI
+{
+    class Pos
+    {
+    public:
+        int x, y;
+    };
+    class Size
+    {
+    public:
+        int w, h;
+    };
+} // namespace GUI
+
+#endif
