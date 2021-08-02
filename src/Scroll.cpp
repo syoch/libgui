09276a7b6cc6664d1423e0ccb45b27b5a6865005
syoch <hacksyoch@gmail.com>
2021-08-03 06:30:12 +0900

    fix: fix Scroll ctor member initalizer order

--- src/Scroll.cpp
+++ src/Scroll.cpp
@@ -1,31 +1,11 @@
-#include <GUI/Scroll.hpp>
 #include <GUI/DrawWrapper.hpp>
+#include <GUI/Scroll.hpp>
 #include <iostream>
 GUI::Scroll::Scroll(int x, int y, int w, int h, bool isHidden)
-    : Container(x, y, w, h, isHidden), xOffset(0), yOffset(0)
-{
-}
-void GUI::Scroll::up(int dy)
-{
-    pos.y -= dy;
-}
-void GUI::Scroll::down(int dy)
-{
-    pos.y += dy;
-}
-void GUI::Scroll::right(int dy)
-{
-    pos.x += dy;
-}
-void GUI::Scroll::left(int dy)
-{
-    pos.y -= dy;
-}
-void GUI::Scroll::horizontal(int dx)
-{
-    pos.x += dx;
-}
-void GUI::Scroll::vertical(int dy)
-{
-    pos.y += dy;
-}
+    : Container(x, y, w, h, isHidden), yOffset(0), xOffset(0) {}
+void GUI::Scroll::up(int dy) { pos.y -= dy; }
+void GUI::Scroll::down(int dy) { pos.y += dy; }
+void GUI::Scroll::right(int dy) { pos.x += dy; }
+void GUI::Scroll::left(int dy) { pos.y -= dy; }
+void GUI::Scroll::horizontal(int dx) { pos.x += dx; }
+void GUI::Scroll::vertical(int dy) { pos.y += dy; }
